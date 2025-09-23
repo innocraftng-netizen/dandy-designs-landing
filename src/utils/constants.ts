7aeda5b2a91530844b5775f0a1a7833a57f1948a
Colman <colman.e@averamax.com>
2025-09-23 23:05:56 +0100

    added email

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,4 @@ export const PHONE = '07012463004' as const;
 export const PHONE2 = '09059180169' as const;
 export const INSTAGRAM = 'https://instagram.com/dandy_designs_fashion_house' as const;
 export const TIKTOK = 'https://tiktok.com/@dandydesignsfashionhouse' as const;
-export const SEND_MAIL_TO = 'email@test.com' as const;
+export const SEND_MAIL_TO = 'adegbuleabigael@gmail.com' as const;
